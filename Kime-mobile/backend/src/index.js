baeaf6d2d00b559fe9df784887970ff32508c394
dfuetesalzmacco <dfuetesalz@maccorobotics.com>
2025-05-14 10:17:04 +0200

    feat: initialize Kime-mobile frontend project with essential files
    - Added package.json with dependencies and scripts for Expo development.
    - Created reset-project.js script to reset project structure and manage existing files.
    - Added tsconfig.json for TypeScript configuration and strict type checking.

---
+++ Kime-mobile/backend/src/index.js
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend funcionando correctamente');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
