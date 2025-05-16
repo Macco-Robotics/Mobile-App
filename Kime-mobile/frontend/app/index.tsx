557ffdf158dd17d6981403d470c586eea0c1ba9e
celiamacco <caguilera@maccorobotics.com>
2025-05-16 09:45:22 +0200

    Refactor code structure for improved readability and maintainability

--- Kime-mobile/frontend/app/index.tsx
+++ Kime-mobile/frontend/app/index.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+import RegistrationForm from "./components/RegistrationForm";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        {/* Logo de Macco */}
+        <Image
+          source={require("../images/logomacco.png")} // Asegúrate de que el logo esté en la carpeta "assets"
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <RegistrationForm />
       </View>
     </View>
   );
@@ -15,20 +22,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#001F3F", // Azul marino
     padding: 24,
   },
   main: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
     maxWidth: 960,
     marginHorizontal: "auto",
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+  logo: {
+    width: 200, // Ajusta el tamaño del logo
+    height: 100,
+    marginBottom: 20, // Espacio entre el logo y el formulario
   },
 });
