94d1c0ae93d14b8632ad1184b863ed7a014eee5a
dfuetesalzmacco <dfuetesalz@maccorobotics.com>
2025-05-20 11:12:53 +0200

    feat: enhance user registration with questionnaire schema and notifications handling

--- Kime-mobile/backend/src/index.js
+++ Kime-mobile/backend/src/index.js
@@ -2,6 +2,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './db/conn.js';
+import authRoutes from './routes/authRoutes.js';
+
 
 dotenv.config();
 const app = express();
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Backend funcionando correctamente');
