61ac794869301ac216872120c89b565440892bf8
dfuetesalzmacco <dfuetesalz@maccorobotics.com>
2025-05-26 09:50:34 +0200

    feat: enhance drink retrieval with error handling and ingredient import

--- Kime-mobile/backend/src/controllers/drinkController.js
+++ Kime-mobile/backend/src/controllers/drinkController.js
@@ -1,8 +1,49 @@
 import Drink from "../models/drink.js";
+import Ingredient from "../models/ingredient.js";
 import User from "../models/user.js";
 
+// This line is for preventing VSCode to delete unused Ingredient import
+void Ingredient;
+
+export const getAllPublishedDrinks = async (req, res) => {
+    try {
+        const drinks = await Drink.find({isPublic: true})
+        .populate('ingredientIds')
+        .populate('creator', 'name surname');
+        res.status(200).json(drinks);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error while retrieving drinks'});
+    }
+}
+
+export const getDrinkById = async (req, res) => {
+    const drinkId  = req.params.id;
+    const userId = req.user.id;
+
+    try {
+        console.log(drinkId)
+        const drink = await Drink.findById(drinkId)
+        .populate('ingredientIds')
+        .populate('creator', 'name surname');
+
+        if (!drink) {
+            return res.status(404).json({message: 'Drink not found'});
+        }
+
+        if(!drink.isPublic && drink.creator.toString() !== userId){
+            return res.status(403).json({message: "You don't have priviledges to see this drink."})
+        }
+
+        res.status(200).json(drink);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error while retrieving drink with id: ',drinkId});
+    }
+}
+
 export const createDrink = async (req, res) => {
-    const { name, description, ingredients, type, creator } = req.body;
+    const { name, description, ingredients, type, creator, isPublic } = req.body;
 
     try {
         // Assign a default image depending on the type of the drink
@@ -23,6 +64,7 @@ export const createDrink = async (req, res) => {
             ingredients,
             type,
             creator,
+            isPublic,
             image
         });
 
--- Kime-mobile/backend/src/routes/drinkRoutes.js
+++ Kime-mobile/backend/src/routes/drinkRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { createDrink } from '../controllers/drinkController.js';
+import { createDrink, getAllPublishedDrinks, getDrinkById } from '../controllers/drinkController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+router.get('/published', getAllPublishedDrinks);
+router.get('/:id', authMiddleware, getDrinkById);
 router.post('/', authMiddleware, createDrink);
 
 export default router;
