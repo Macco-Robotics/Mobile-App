da5de87a2e21557f1197455f8fa893d54266b1dc
dfuetesalzmacco <dfuetesalz@maccorobotics.com>
2025-06-02 09:49:23 +0200

    refactor: remove unused authRoutes import and middleware

--- Kime-mobile/backend/src/index.js
+++ Kime-mobile/backend/src/index.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { connectDB } from './db/conn.js';
-import authRoutes from './routes/authRoutes.js';
+
 import drinkRoutes from './routes/drinkRoutes.js';
 import inventoryRoutes from './routes/inventoryRoutes.js';
 import menuRoutes from './routes/menuRoutes.js';
@@ -15,8 +15,6 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/auth', authRoutes);
 app.use('/api/drink', drinkRoutes);
 app.use('/api/menu', menuRoutes);
 
